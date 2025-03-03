73858a9f3587c10b3fb563ee12057ecd08733a6b
gao.brian <gao.brian@gmail.com>
2025-03-03 10:51:10 +0800

    Update help info

--- cmd/spire-agent/cli/validate/validate.go
+++ cmd/spire-agent/cli/validate/validate.go
@@ -28,15 +28,15 @@ func (c *validateCommand) Help() string {
 }
 
 func (c *validateCommand) Synopsis() string {
-	return "Validates a SPIRE agent configuration file"
+	return "Validates agent configuration file"
 }
 
 func (c *validateCommand) Run(args []string) int {
 	if _, err := run.LoadConfig(commandName, args, nil, c.env.Stderr, false); err != nil {
 		// Ignore error since a failure to write to stderr cannot very well be reported
-		_ = c.env.ErrPrintf("SPIRE agent configuration file is invalid: %v\n", err)
+		_ = c.env.ErrPrintf("Kirin agent configuration file is invalid: %v\n", err)
 		return 1
 	}
-	_ = c.env.Println("SPIRE agent configuration file is valid.")
+	_ = c.env.Println("Kirin agent configuration file is valid.")
 	return 0
 }
